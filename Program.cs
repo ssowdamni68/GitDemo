0b571f7782cc67eaf4243b401a098451cec940a1
ssowdamini68 <ssowdamini68@gmail.com>
2021-11-11 15:41:10 +0530

    new changes

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace GitDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello it's sowda!");
+            Console.WriteLine("Hello it's sowdamini!");
         }
     }
 }
